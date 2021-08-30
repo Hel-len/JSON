79a38574935d071ded91de8a4cca195c93a284c6
Lena <eveforeve@yandex.ru>
2021-08-30 19:42:13 +0500

    Initial Commit

---
+++ JSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSON
+//
+//  Created by Елена Дранкина on 30.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSON/JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONApp.swift
+//  JSON
+//
+//  Created by Елена Дранкина on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
